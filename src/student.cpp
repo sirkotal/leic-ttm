ab4135a2b708c6183c13d419ea240ee3c15309b8
sirkotal <lordkotal2002@gmail.com>
2022-11-03 17:33:22 +0000

    Cosmetic fixes to ClassSchedule

--- src/student.cpp
+++ src/student.cpp
@@ -43,7 +43,7 @@ string toTime(float time) {
 void Student::showSchedule() const {
     for (Slot element: schedule) {
         cout << element.getUC() << "-> " << element.getDay() << ": " << toTime(element.getStart()) << "->" << toTime(element.getEnd()) << ", " << element.isType() << endl;
-        cout << "|-------------------------------|" << endl;
+        cout << "|------------------------------------|" << endl;
     }
 }
 
